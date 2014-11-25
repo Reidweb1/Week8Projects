558844c0b4deb9bcd76f0718c6d82c837875a2f3
Reid Weber <reid_weber@hotmail.com>
2014-11-25 12:15:38 -0800

    Initial Commit

---
+++ BurgerMenuObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BurgerMenuObjC
+//
+//  Created by Reid Weber on 11/25/14.
+//  Copyright (c) 2014 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BurgerMenuObjC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BurgerMenuObjC
+//
+//  Created by Reid Weber on 11/25/14.
+//  Copyright (c) 2014 Reid Weber. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
