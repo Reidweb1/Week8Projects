01651be57877d247fdaf83db8f8844aa495317a4
Reid Weber <reid_weber@hotmail.com>
2014-11-25 13:34:15 -0800

    Removed ARC

--- BurgerMenuObjC/ViewController.m
+++ BurgerMenuObjC/ViewController.m
@@ -7,21 +7,72 @@
 //
 
 #import "ViewController.h"
+#import "MenuTableViewCell.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UIView *backgroundTableView;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *tableViewLeadingContraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *backgroundViewLeadingConstraint;
+@property (strong, nonatomic) IBOutlet UIButton *menuButton;
+@property (strong, nonatomic) UIViewController *blackViewController;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.view sendSubviewToBack: self.menuButton];
+    self.blackViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"BLACK_VC"];
+    [self addChildViewController:self.blackViewController];
+    self.blackViewController.view.frame = self.view.frame;
+    [self.view insertSubview: self.blackViewController.view belowSubview:self.menuButton];
+    [self.blackViewController didMoveToParentViewController:self];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.tableViewLeadingContraint.constant = -216;
+    self.backgroundViewLeadingConstraint.constant = -166;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)menuButtonClicked:(id)sender {
+    self.tableViewLeadingContraint.constant = -16;
+    self.backgroundViewLeadingConstraint.constant = -50;
+    [UIView animateWithDuration:1.0 delay:0.0 usingSpringWithDamping:0.75 initialSpringVelocity:0.75 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        [self.view layoutIfNeeded];
+    } completion:nil];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MenuTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"MENU_CELL"];
+    cell.mainLabel.text = @"Menu";
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    self.tableViewLeadingContraint.constant = -216;
+    self.backgroundViewLeadingConstraint.constant = -166;
+    [UIView animateWithDuration:0.5 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)dealloc {
+    [super dealloc];
+    [_backgroundTableView release];
+    [_backgroundViewLeadingConstraint release];
+    [_tableView release];
+    [_tableViewLeadingContraint release];
+    [_menuButton release];
+    [_blackViewController release];
 }
 
 @end
