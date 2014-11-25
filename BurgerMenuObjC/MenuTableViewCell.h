64486f17829f2ac022cfaf6ab8857e424408400d
Reid Weber <reid_weber@hotmail.com>
2014-11-25 13:12:30 -0800

    Added Menu With ANimations

---
+++ BurgerMenuObjC/MenuTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewCell.h
+//  BurgerMenuObjC
+//
+//  Created by Reid Weber on 11/25/14.
+//  Copyright (c) 2014 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewCell : UITableViewCell
+
+@end
